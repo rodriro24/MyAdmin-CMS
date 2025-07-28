71bcd0cad8f4ae12e1ce147cb2772c06556ad892
Rodrigo <rodrirolon.rl@gmail.com>
2025-07-28 13:48:54 -0300

    first commit

---
+++ src/app/auth/register/page.tsx
@@ -0,0 +1,14 @@
+import { BookmarkIcon } from '@radix-ui/react-icons'
+import {Button} from '@radix-ui/themes'
+const page = () => {
+  return (
+    <div>
+      <h1>Register</h1>
+      <Button >
+        <BookmarkIcon /> Click here to register
+      </Button>
+    </div>
+  )
+}
+
+export default page
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,10 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import "@radix-ui/themes/styles.css";
+import { Theme } from "@radix-ui/themes";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,9 +19,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`antialiased`}
       >
-        {children}
+        <Theme>
+          {children}
+        </Theme>
       </body>
     </html>
   );
