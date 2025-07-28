42ecc85e54f14862ea3746d2b3605992181dd064
Rodrigo <rodrirolon.rl@gmail.com>
2025-07-28 16:55:27 -0300

    forms

--- src/components/auth/RegisterForm.tsx
+++ src/components/auth/RegisterForm.tsx
@@ -1,57 +1,114 @@
-// "use client"
-import '@radix-ui/themes/styles.css'
-import { EnvelopeClosedIcon, LockClosedIcon, PersonIcon } from '@radix-ui/react-icons'
-import { Flex, TextField, Button } from '@radix-ui/themes'
-import React from 'react'
+"use client";
+import "@radix-ui/themes/styles.css";
+import {
+  EnvelopeClosedIcon,
+  LockClosedIcon,
+  PersonIcon,
+} from "@radix-ui/react-icons";
+import { Flex, TextField, Button } from "@radix-ui/themes";
+import React, { useState } from "react";
 
 const RegisterForm = () => {
+  const [form, setForm] = useState({
+    name: "",
+    lastname: "",
+    nick: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log(form); // lógica para registrar
+  };
+
   return (
-    <Flex direction="column" gap="3" className=' my-4' >
-      <label htmlFor="name">Name</label>
-      <TextField.Root id="name" placeholder='Your name' type='text' autoFocus>
-        <TextField.Slot>
-          <PersonIcon height="16" width="16" />
-        </TextField.Slot>
-      </TextField.Root>
+    <form onSubmit={handleSubmit}>
+      <Flex direction="column" gap="3" className="my-4 mx-auto" width="300px">
+        <label htmlFor="name">Name</label>
+        <TextField.Root id="name"
+            name="name"
+            placeholder="Your name"
+            type="text"
+            value={form.name}
+            onChange={handleChange}
+            autoFocus>
+          <TextField.Slot>
+            <PersonIcon height="16" width="16" />
+          </TextField.Slot>
+        </TextField.Root>
 
-      <label htmlFor="last">Lastname</label>
-      <TextField.Root id="last" placeholder='Your lastname' type='text'>
-        <TextField.Slot>
-          <PersonIcon height="16" width="16" />
-        </TextField.Slot>
-      </TextField.Root>
+        <label htmlFor="lastname">Lastname</label>
+        <TextField.Root id="lastname"
+            name="lastname"
+            placeholder="Your lastname"
+            type="text"
+            value={form.lastname}
+            onChange={handleChange}>
+          <TextField.Slot>
+            <PersonIcon height="16" width="16" />
+          </TextField.Slot>
+        </TextField.Root>
 
-      <label htmlFor="nick">Nick</label>
-      <TextField.Root id="name" placeholder='Type a nick' type='text'>
-        <TextField.Slot>
-          <PersonIcon height="16" width="16" />
-        </TextField.Slot>
-      </TextField.Root>
+        <label htmlFor="nick">Nick</label>
+        <TextField.Root id="nick"
+            name="nick"
+            placeholder="Type a nick"
+            type="text"
+            value={form.nick}
+            onChange={handleChange}>
+          <TextField.Slot>
+            <PersonIcon height="16" width="16" />
+          </TextField.Slot>
+        </TextField.Root>
 
-      <label htmlFor="email">Email</label>
-      <TextField.Root id="email" placeholder='email@domain.com' type='email'>
-        <TextField.Slot>
-          <EnvelopeClosedIcon height="16" width="16" />
-        </TextField.Slot>
-      </TextField.Root>
+        <label htmlFor="email">Email</label>
+        <TextField.Root id="email"
+            name="email"
+            placeholder="email@domain.com"
+            type="email"
+            value={form.email}
+            onChange={handleChange}>
+          <TextField.Slot>
+            <EnvelopeClosedIcon height="16" width="16" />
+          </TextField.Slot>
+        </TextField.Root>
 
-      <label htmlFor="pass">Password</label>
-      <TextField.Root id="pass" placeholder='Your password' type='password'>
-        <TextField.Slot>
-          <LockClosedIcon height="16" width="16" />
-        </TextField.Slot>
-      </TextField.Root>
-      <TextField.Root id="passRe" placeholder='Retype your password' type='password'>
-        <TextField.Slot>
-          <LockClosedIcon height="16" width="16" />
-        </TextField.Slot>
-      </TextField.Root>
+        <label htmlFor="password">Password</label>
+        <TextField.Root id="password"
+            name="password"
+            placeholder="Your password"
+            type="password"
+            value={form.password}
+            onChange={handleChange}>
+          <TextField.Slot>
+            <LockClosedIcon height="16" width="16" />
+          </TextField.Slot>
+        </TextField.Root>
 
-      <Button size="1" variant="solid" color="blue">
-        Sign Up
-      </Button>
-    </Flex>
-  )
-}
+        <label htmlFor="confirmPassword">Confirm Password</label>
+        <TextField.Root id="confirmPassword"
+            name="confirmPassword"
+            placeholder="Retype your password"
+            type="password"
+            value={form.confirmPassword}
+            onChange={handleChange}>
+          <TextField.Slot>
+            <LockClosedIcon height="16" width="16" />
+          </TextField.Slot>
+        </TextField.Root>
 
-export default RegisterForm
+        <Button color="blue" type="submit">
+          Sign Up
+        </Button>
+      </Flex>
+    </form>
+  );
+};
+
+export default RegisterForm;
--- src/components/auth/SignInForm.tsx
+++ src/components/auth/SignInForm.tsx
@@ -1,31 +1,55 @@
-// "use client"
-import '@radix-ui/themes/styles.css'
-import { EnvelopeClosedIcon, LockClosedIcon } from '@radix-ui/react-icons'
-import { Flex, TextField, Button } from '@radix-ui/themes'
-import React from 'react'
+"use client";
+import "@radix-ui/themes/styles.css";
+import { EnvelopeClosedIcon, LockClosedIcon } from "@radix-ui/react-icons";
+import { Flex, TextField, Button } from "@radix-ui/themes";
+import React, { useState } from "react";
 
 const SignInForm = () => {
+  const [form, setForm] = useState({ email: "", password: "" });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log(form); // acá iría tu lógica de auth
+  };
+
   return (
-    <Flex direction="column" gap="3" className=' my-4' >
-      <label htmlFor="email">Email</label>
-      <TextField.Root id="email" placeholder='email@domain.com' type='email' autoFocus>
-        <TextField.Slot>
-          <EnvelopeClosedIcon height="16" width="16" />
-        </TextField.Slot>
-      </TextField.Root>
+    <form onSubmit={handleSubmit} className="">
+      <Flex direction="column" gap="3" className="my-4 mx-auto" width="300px">
+        <label htmlFor="email">Email</label>
+        <TextField.Root id="email"
+            name="email"
+            placeholder="email@domain.com"
+            type="email"
+            autoFocus
+            value={form.email}
+            onChange={handleChange}>
+          <TextField.Slot>
+            <EnvelopeClosedIcon height="16" width="16" />
+          </TextField.Slot>
+        </TextField.Root>
 
-      <label htmlFor="pass">Password</label>
-      <TextField.Root id="pass" placeholder='********' type='password'>
-        <TextField.Slot>
-          <LockClosedIcon height="16" width="16" />
-        </TextField.Slot>
-      </TextField.Root>
+        <label htmlFor="password">Password</label>
+        <TextField.Root id="password"
+            name="password"
+            placeholder="*******"
+            type="password"
+            value={form.password}
+            onChange={handleChange}>
+          <TextField.Slot>
+            <LockClosedIcon height="16" width="16" />
+          </TextField.Slot>
+        </TextField.Root>
 
-      <Button size="1" variant="solid" color="blue">
-        Sign In
-      </Button>
-    </Flex>
-  )
-}
+        <Button color="blue" type="submit">
+          Sign In
+        </Button>
+      </Flex>
+    </form>
+  );
+};
 
-export default SignInForm
+export default SignInForm;
