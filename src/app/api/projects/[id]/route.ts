6f5729427f9c790b1a620e3a9e4b02128a6240fe
Rodrigo <rodrirolon.rl@gmail.com>
2025-08-01 00:44:09 -0300

    idk

--- src/app/api/projects/[id]/route.ts
+++ src/app/api/projects/[id]/route.ts
@@ -1,31 +1,29 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/libs/prisma";
 
-// GET /api/projects/[id]
-export async function GET(
-  request: Request,
-  context: { params: { id: string } }
-) {
-  const id = Number(context.params.id);
+type Context = {
+  params: {
+    id: string;
+  };
+};
+
+export async function GET(_req: NextRequest, { params }: Context) {
+  const id = Number(params.id);
 
   try {
-    const res = await prisma.project.findUnique({ where: { id } });
-    if (!res) {
+    const project = await prisma.project.findUnique({ where: { id } });
+    if (!project) {
       return NextResponse.json({ error: "Project not found" }, { status: 404 });
     }
-    return NextResponse.json(res);
+    return NextResponse.json(project);
   } catch (error) {
     console.error(error);
     return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
   }
 }
 
-// DELETE /api/projects/[id]
-export async function DELETE(
-  request: Request,
-  context: { params: { id: string } }
-) {
-  const id = Number(context.params.id);
+export async function DELETE(_req: NextRequest, { params }: Context) {
+  const id = Number(params.id);
 
   try {
     await prisma.project.delete({ where: { id } });
@@ -36,20 +34,13 @@ export async function DELETE(
   }
 }
 
-// PUT /api/projects/[id]
-export async function PUT(
-  request: Request,
-  context: { params: { id: string } }
-) {
-  const id = Number(context.params.id);
-  const data = await request.json();
+export async function PUT(req: NextRequest, { params }: Context) {
+  const id = Number(params.id);
+  const data = await req.json();
 
   try {
-    const updatedProject = await prisma.project.update({
-      where: { id },
-      data,
-    });
-    return NextResponse.json(updatedProject);
+    const updated = await prisma.project.update({ where: { id }, data });
+    return NextResponse.json(updated);
   } catch (error) {
     console.error(error);
     return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
