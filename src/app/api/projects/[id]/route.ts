7a880b6fc0bdf70db6958e7fdc8058c5109b84ac
Rodrigo <rodrirolon.rl@gmail.com>
2025-08-01 00:34:00 -0300

    fixed again

--- src/app/api/projects/[id]/route.ts
+++ src/app/api/projects/[id]/route.ts
@@ -1,11 +1,12 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/libs/prisma";
 
+// GET /api/projects/[id]
 export async function GET(
   request: Request,
-  { params }: { params: { id: string } }
+  context: { params: { id: string } }
 ) {
-  const id = Number(params.id);
+  const id = Number(context.params.id);
 
   try {
     const res = await prisma.project.findUnique({ where: { id } });
@@ -15,30 +16,32 @@ export async function GET(
     return NextResponse.json(res);
   } catch (error) {
     console.error(error);
-    return NextResponse.error();
+    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
   }
 }
 
+// DELETE /api/projects/[id]
 export async function DELETE(
   request: Request,
-  { params }: { params: { id: string } }
+  context: { params: { id: string } }
 ) {
-  const id = Number(params.id);
+  const id = Number(context.params.id);
 
   try {
     await prisma.project.delete({ where: { id } });
     return NextResponse.json({ message: "Project successfully deleted" });
   } catch (error) {
     console.error(error);
-    return NextResponse.error();
+    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
   }
 }
 
+// PUT /api/projects/[id]
 export async function PUT(
   request: Request,
-  { params }: { params: { id: string } }
+  context: { params: { id: string } }
 ) {
-  const id = Number(params.id);
+  const id = Number(context.params.id);
   const data = await request.json();
 
   try {
@@ -49,6 +52,6 @@ export async function PUT(
     return NextResponse.json(updatedProject);
   } catch (error) {
     console.error(error);
-    return NextResponse.error();
+    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
   }
 }
