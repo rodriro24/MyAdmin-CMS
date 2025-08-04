adb810a27e9b9ee83ac02d6694bb070d344ac9cc
Rodrigo <rodrirolon.rl@gmail.com>
2025-08-04 14:55:20 -0300

    Update NEXTAUTH_URL in Vercel

--- src/libs/prisma.ts
+++ src/libs/prisma.ts
@@ -4,6 +4,6 @@ import { PrismaClient } from "@prisma/client";
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
-  globalForPrisma.prisma || new PrismaClient();
+  globalForPrisma.prisma || new PrismaClient();;
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
